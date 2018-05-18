5decd562bd5710d9775c8eefcf04446ecf9a7a13
seb_bouba89 <bouba59@free.fr>
2018-05-18 14:55:56 +0200

    change index en php et header
    passage en .php

--- Projet_Groupe-front/includes/header.php
+++ Projet_Groupe-front/includes/header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,29 +23,5 @@
             </div>
     </div>
     </nav>
-    </div>
-    <div class="container-fluid">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-lg-3 mt-4">
-                    <div class="card-group">
-                        <div class="card"><img class="card-img-top w-100 d-block" src="assets/img/images.jpg">
-                            <div class="card-body"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 offset-3 mt-4">
-                    <h2>Mes derniers articles ou pages</h2>
-                    <p>Paragraph</p>
-                    <p>Paragraph</p>
-                    <p>Paragraph</p>
-                    <p>Paragraph</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/js/bootstrap.bundle.min.js"></script>
-</body>
 
-</html>
+
