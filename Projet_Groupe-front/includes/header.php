9445acd0d114e498855c98f23168c6c9ad40caaa
seb_bouba89 <38311664+sebastien-dujardin@users.noreply.github.com>
2018-05-18 17:28:42 +0200

    ajout modal
    ajout modal bootstrap header.php

--- Projet_Groupe-front/includes/header.php
+++ Projet_Groupe-front/includes/header.php
@@ -6,6 +6,8 @@
     <title>communautée de commune</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/styles.min.css">
+    <script type="text/javascript" src="assets/js/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
 </head>
 
 <body>
@@ -19,9 +21,48 @@
                         <li class="nav-item" role="presentation"><a class="nav-link" href="mon_profil.html">Mon profil</a></li>
                         <li class="nav-item" role="presentation"><a class="nav-link" href="#">Mes articles</a></li>
                         <li class="nav-item" role="presentation"><a class="nav-link" href="#">Créer un article</a></li>
+                        <li class="nav-item active">
+                    <a data-toggle="modal" data-target="#exampleModalCenter" href="#register" >Login<span class="sr-only">(current)</span></a> 
+                </li>
                     </ul>
+
             </div>
     </div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4><span class="glyphicon glyphicon-lock"></span> Login</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form role="form">
+            <div class="form-group">
+              <label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
+              <input type="text" class="form-control" id="usrname" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+              <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+              <input type="text" class="form-control" id="psw" placeholder="Enter password">
+            </div>
+            <div class="checkbox">
+              <label><input type="checkbox" value="" checked>Remember me</label>
+            </div>
+              <button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>
+          </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
     </nav>
 
 
