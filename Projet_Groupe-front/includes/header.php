24f445ab7e124e973fcb70459637b512644bd17c
ljdj <ljdj89@gmail.com>
2018-05-22 13:48:36 +0200

    correction lien navbar

--- Projet_Groupe-front/includes/header.php
+++ Projet_Groupe-front/includes/header.php
@@ -1,6 +1,6 @@
 <header>
   <nav class="navbar navbar-light navbar-expand-md navigation-clean">
-    <div class="container"><a class="navbar-brand" href="#">Communication Auxerroise</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    <div class="container"><a class="navbar-brand" href="accueil.php">Communication Auxerroise</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
       <div class="collapse navbar-collapse" id="navcol-1">
         <ul class="nav navbar-nav ml-auto">
           <li class="nav-item" role="presentation"><a class="nav-link active" href="mon_profil.php">Mon profil</a></li>
