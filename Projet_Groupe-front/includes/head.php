52154459cc9383dc5911d0c37801c8b2ea0dbc1d
ljdj <ljdj89@gmail.com>
2018-05-22 10:12:52 +0200

    modif liens

--- Projet_Groupe-front/includes/head.php
+++ Projet_Groupe-front/includes/head.php
@@ -4,11 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>communautée de commune</title>
     <!-- CSS -->
-	<link rel="stylesheet" href="../assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" href="assets/css/styles.min.css">
     <!-- Javascript -->
-	<script src="../assets/js/jquery-3.3.1.js"></script>
-	<script src="../assets/js/bootstrap.js"></script>
+	<script src="assets/js/jquery-3.3.1.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
 
 
 </head>
