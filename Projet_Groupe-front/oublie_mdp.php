243061427116cb82a74db0e619173e116b988022
jujuquatorze <chevrierjulien89@gmail.com>
2018-05-22 15:41:24 +0200

    Ajout du MDP oublié
    Ajout du systeme de token

--- Projet_Groupe-front/oublie_mdp.php
+++ Projet_Groupe-front/oublie_mdp.php
@@ -1,35 +1,38 @@
 <?php
-    include('includes/head.php');
-	include('includes/header.php');
-	require_once('assets/php/inc_bdd.php')
+include('includes/head.php');
+include('includes/header.php');
 
 ?>
-	<body>
-		<section class="container">
-			<div class="row"">
-				<div class="col mt-3 py-2">
-						<h1 id="title">Mot de passe oublié</h1>
-					</div>						
-				<div class="input-group mb-3">						
-					<div class="input-group mb-3" id="oubli">
-					  	<div class="input-group-prepend">
-					    <span class="input-group-text" id="basic-addon1"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-					</div>
-					<input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">							
-				<button type="submit" class="bgVlt">Envoyer</button>
-							</div>
-				</div>
-				<div class="col col-lg-3 mt-4" >
-	                <div class="card-group">
-	                    <div class="card"><img class="card-img-top w-100 d-block" src="assets/img/images.jpg">
-	                        <div class="card-body"></div>
-	                    </div>
-	            	</div>
-	            </div>
+<body>
+	<section class="container">
+		<div class="row"">
+			<div class="col mt-3 py-2">
+				<h1 id="title">Mot de passe oublié</h1>
 			</div>
-		</section>	
+		</div>
+		<form method="POST" action="assets/php/systeme_oublie_mdp.php">
+			<div class="input-group mb-3">						
+				<div class="input-group mb-3" id="oubli">
+					<div class="input-group-prepend">
+						<span class="input-group-text" id="basic-addon1"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+					</div>
+					<input type="email" name="mail" class="form-control" placeholder="Mettez votre Email" aria-label="Username" aria-describedby="basic-addon1">							
+
+				</div>
+				<button type="submit" class="bgVlt">Envoyer</button>
+			</div>
+		</form>
+		<div class="col col-lg-3 mt-4" >
+			<div class="card-group">
+				<div class="card"><img class="card-img-top w-100 d-block" src="assets/img/images.jpg">
+					<div class="card-body"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>	
 <?php
 
-	include('includes/footer.php');
+include('includes/footer.php');
 
 ?>
