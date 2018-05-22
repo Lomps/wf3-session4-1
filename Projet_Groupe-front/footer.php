eb35851025d44a5bff9fa586b393df0aa1de142a
Lomps <38312954+Lomps@users.noreply.github.com>
2018-05-22 12:30:38 +0200

    Add files via upload
    mise en responsive du header et du footer, de plus ajout des logos fontawesome sur la modal

--- Projet_Groupe-front/footer.php
+++ Projet_Groupe-front/footer.php
@@ -1,21 +1,23 @@
 <footer>
-             <div class="container-fluid">
-                <div class="row">
-                    <div class="col ftr">
-                    <ul class="nav justify-content-center">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="credit.html">Crédit</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="mentions-legales.html">Mentions légales</a>                 
-                         <li class="nav-item">
-                            <a class="nav-link" href="contact_us.html">Contacter Nous</a>
-                        </li>
-                    </ul>
-                    <!--<p class="copy mb-0"><i class="far fa-copyright"></i> 2018 WebForce3</p>  -->               
-                    </div>
-                </div>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/fontawesome-all.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 ">
+                <ul class="nav justify-content-center ftr">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="credit.html">Crédit</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="mentions-legales.html">Mentions légales</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact_us.html">Contacter Nous</a>
+                    </li>
+                </ul>
+                <p class="copy mb-0 ftr">&copy 2018 WebForce3</p>
             </div>
-        </footer> <!-- /Copyright -->    
-    </body>
-</html>
+        </div>
+    </div>
+</footer>
