0cb91be1f32f517018554e3aa9d650e09a0554ff
seb_bouba89 <bouba59@free.fr>
2018-05-23 15:53:40 +0200

    recuperation info champ input
    changement  mon_profil.php

--- Projet_Groupe-front/mon_profil.php
+++ Projet_Groupe-front/mon_profil.php
@@ -1,9 +1,28 @@
 <!DOCTYPE html>
 <html>
 <?php
+session_start();
 include('includes/head.php');
 include('includes/header.php');
 require_once('assets/php/inc_bdd.php');
+
+if (isset($_SESSION['id']))
+{
+$id =  strip_tags($_SESSION['id']);
+$sql = " SELECT * FROM utilisateur WHERE id_utilisateur = :id";
+
+$query = $db->prepare($sql);
+
+$query -> bindValue(":id", $id , PDO::PARAM_INT);
+$query-> execute();
+
+$result = $query->fetch();
+
+}
+
+
+
+
 ?>
 
 <body>
@@ -19,23 +38,24 @@ require_once('assets/php/inc_bdd.php');
                 </div>
                 <div class="col col-sm-6 mt-4">
                     <h2>Modifier mes informations</h2>
+                    <form method="POST" action="edite_profil.php">
                     <div class="input-group">
-                        <div class="input-group-prepend"><span class="input-group-text" id="info">Nom</span></div><input class="form-control" type="text" name="nom">
+                        <div class="input-group-prepend"><span class="input-group-text" id="info">Nom</span></div><input class="form-control" type="text" name="nom" value="<?= $result['nom'] ?>">
                         
                     </div>
                     <div class="input-group">
-                        <div class="input-group-prepend"><span class="input-group-text" id="adr">Prénom</span></div><input class="form-control" type="text" name="prenon">
+                        <div class="input-group-prepend"><span class="input-group-text" id="adr">Prénom</span></div><input class="form-control" type="text" name="prenom" value="<?= $result['prenom'] ?>">
                         
                     </div>
                     <div class="input-group">
-                        <div class="input-group-prepend"><span class="input-group-text" id="Cd">Email</span></div><input class="form-control" type="email" name="email">
+                        <div class="input-group-prepend"><span class="input-group-text" id="Cd">Email</span></div><input class="form-control" type="email" name="email" value="<?= $result['email'] ?>">
                         
                     </div>
                     <div class="input-group">
-                        <div class="input-group-prepend"><span class="input-group-text" id="password">Mot de passe</span></div><input class="form-control" type="password">                        
+                        <div class="input-group-prepend"><span class="input-group-text" id="password">Mot de passe</span></div><input class="form-control" type="password" name="password">                        
                     </div>
-                    <div><p></p></div>
                     <div class="input-group-append"><button class="btn btn-primary bgVlt" type="button">Modifier !</button></div>
+                </form>
                 </div>
             </div>
         </div>
