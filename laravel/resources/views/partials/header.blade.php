e4b8fdd60b96d3911ca7ee0c0a55abeb4e08a47d
ljdj <ljdj89@gmail.com>
2018-06-02 17:52:57 +0200

    Ajout commentaire sur header

--- laravel/resources/views/partials/header.blade.php
+++ laravel/resources/views/partials/header.blade.php
@@ -17,58 +17,71 @@
 <body>
 	<nav class="navbar navbar-expand-lg navbar-dark  bg-dark">
 		<section class="container">
-			<a class="navbar-brand" href="{{ URL::to('/') }}"><img src="{{ asset('assets/img/images.jpg') }}" style="height: 40px"></a>
+			{{-- lien sur logo --}}
+			<a class="navbar-brand" href="{{ URL::to('/') }}">
+				{{-- logo --}}
+				<img src="{{ asset('assets/img/images.jpg') }}" style="height: 40px">
+			</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mr-auto">
+					{{-- lien Accueil --}}
 					<li class="nav-item active">
-						<a class="nav-link{{(Route::currentRouteName() == 'accueil') ? ' active': ''}}">
-							<a class="nav-link" href="{{URL::to('/')}}">
-								{{ __('Home')}}<span class="sr-only">(current)</span></a>
-							</li>		      
-						</ul>
-						<ul class="navbar-nav ml-auto">
-							<!-- Authentication Links -->                
-							@guest
-							<li class="nav-item{{(Route::currentRouteName() == 'login') ? ' active': ''}}">
-								<a class="nav-link" href="{{ route('login') }}">
-									{{ __('Login') }}
-								</a>
-							</li>
-							<li class="{{(Route::currentRouteName() == 'register') ? ' active': ''}}">
-								<a class="nav-link" href="{{ route('register') }}">
-									{{ __('Register') }}
-								</a>
-							</li>
-							@else
-							@if(Auth::user()->role==4)
-							<li class="nav-item{{ (Route::getCurrentRoute()->getPrefix() == '/admin') ? ' active': ''}}">
-								<a class='nav-link' href="{{ route('admin')}}">
-									{{ __('Admin') }}
-								</a>
-							</li>
-							@endif
-							<li class="nav-item dropdown">
-								<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-									{{ Auth::user()->name }} <span class="caret"></span>
-								</a>
+						<a class="nav-link" href="{{URL::to('/')}}">
+							{{ __('Home')}}
+							<span class="sr-only">(current)</span>
+						</a>
+					</li>
+				</ul>
+				<ul class="navbar-nav ml-auto">
+					<!-- Authentication Links -->
+					@guest
+					{{-- lien login --}}
+					<li class="nav-item{{(Route::currentRouteName() == 'login') ? ' active': ''}}">
+						<a class="nav-link" href="{{ route('login') }}">
+							{{ __('Login') }}
+						</a>
+					</li>
+					{{-- lien register --}}
+					<li class="{{(Route::currentRouteName() == 'register') ? ' active': ''}}">
+						<a class="nav-link" href="{{ route('register') }}">
+							{{ __('Register') }}
+						</a>
+					</li>
+					@else
+					@if(Auth::user()->role==4)
+					{{-- lien admin si role 4 --}}
+					<li class="nav-item{{ (Route::getCurrentRoute()->getPrefix() == '/admin') ? ' active': ''}}">
+						<a class='nav-link' href="{{ route('admin')}}">
+							{{ __('Admin') }}
+						</a>
+					</li>
+					@endif
+					<li class="nav-item dropdown">
+						{{-- lien menu déroulant --}}
+						<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+							{{ Auth::user()->name }} 
+							<span class="caret"></span>
+						</a>
 
-								<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-									<a class="dropdown-item" href="{{ route('profil') }}">{{ __('Profil') }}</a>
-									<a class="dropdown-item" href="{{ route('logout') }}"
-									onclick="event.preventDefault();
-									document.getElementById('logout-form').submit();">
-									{{ __('Déconnexion') }}
-								</a>
-								<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-									@csrf
-								</form>
-							</div>
-						</li>
-						@endguest
-					</ul>
-				</div>
-			</section>
-		</nav>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							{{-- lien profil --}}
+							<a class="dropdown-item" href="{{ route('profil') }}">
+								{{ __('Profil') }}
+							</a>
+							{{-- lien logout --}}
+							<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+								{{ __('Déconnexion') }}
+							</a>
+							<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+								@csrf
+							</form>
+						</div>
+					</li>
+					@endguest
+				</ul>
+			</div>
+		</section>
+	</nav>
