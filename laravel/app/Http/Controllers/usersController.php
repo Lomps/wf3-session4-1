1090da7720c3959fd3aea998ad20db5e38f6f339
seb_bouba89 <38311664+sebastien-dujardin@users.noreply.github.com>
2018-06-06 18:19:04 +0200

    model article
    j'ai ajouter le articleModel et modifié le usersController pour ajouter ce model

--- laravel/app/Http/Controllers/usersController.php
+++ laravel/app/Http/Controllers/usersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\mairieModel as Mairie;
+use App\articleModel as article;
 use Auth;
 class usersController extends Controller
 {
