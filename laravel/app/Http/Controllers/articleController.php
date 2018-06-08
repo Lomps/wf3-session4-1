98eac6f1a2db743b5e54e29db3ee6335c1ed30be
sebastien-dujardin <bouba59@free.fr>
2018-06-08 11:37:29 +0200

    rajout du champ id_publication dans le controller article

--- laravel/app/Http/Controllers/articleController.php
+++ laravel/app/Http/Controllers/articleController.php
@@ -30,6 +30,7 @@ class articleController extends Controller
 			$article->contenu = $donnees['contenu'];
 			$article->zone_idzone = $donnees['id_zone'];
 			$article->users_iduser = Auth::User()->iduser;
+			$article->publication =$donnees['publication_id_publication'];
 			$article->nom_image = $logoPath;
 			$article->save();
 			return redirect()->back()->with('message','Votre article est crée avec succès !');
