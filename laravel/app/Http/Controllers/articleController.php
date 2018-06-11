9c94b6b95c3af177dc3e49c5bcf97c1560520d59
sebastien-dujardin <bouba59@free.fr>
2018-06-11 16:34:35 +0200

    modification

--- laravel/app/Http/Controllers/articleController.php
+++ laravel/app/Http/Controllers/articleController.php
@@ -17,13 +17,11 @@ class articleController extends Controller
 
     public function ajoutarticle(Request $donnees) {
 		$validateData = $donnees->validate([
-			'titre' => 'required|max:255',
+			'contenu' => 'required|max:255',
 			'type' => 'required',
 			'nom_image' => 'dimensions:max_width=300,max_height=300',
-			'nom_signature' => 'max:255',
 			'zone_id_zone' => 'required',
 			'publication_id_publication' => 'required',
-			'zone_id_zone' => 'required'
 		]);
 
 		if ($donnees->hasFile('logo')) {
@@ -32,10 +30,9 @@ class articleController extends Controller
 			}
 			$id = $donnees['id_contenu'];
 			$article = new Article();
-			$article->titre = $donnees['titre'];
+			$article->contenu = $donnees['contenu'];
 			$article->type = $donnees['type'];
 			$article->nom_image = $logoPath;
-			$article->nom_signature = $donnees['nom_signature'];
 			$article->zone_id_zone = $donnees['zone_id_zone'];
 			$article->publication_id_publication = $donnees['publication_id_publication'];
 			$article->users_iduser = Auth::User()->iduser;
