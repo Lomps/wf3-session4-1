6f49a21e524e66fdf34bce531bbd7e209b55c7e6
sebastien-dujardin <bouba59@free.fr>
2018-06-12 09:27:21 +0200

    ajout zonemodel et function zone dans articlecontroller

--- laravel/app/Http/Controllers/articleController.php
+++ laravel/app/Http/Controllers/articleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\articleModel as Articles;
 use App\ajoutarticleModel as Article;
+use App\zoneModel as Zone;
 
 class articleController extends Controller
 {
@@ -14,6 +15,8 @@ class articleController extends Controller
 		return view('articles', ['articles' => $articles]);
 
 	}
+	
+// valide l'ajout d'un article
 
     public function ajoutarticle(Request $donnees) {
 		$validateData = $donnees->validate([
@@ -32,11 +35,19 @@ class articleController extends Controller
 			$article = new Article();
 			$article->contenu = $donnees['contenu'];
 			$article->type = $donnees['type'];
+			$article->
 			$article->nom_image = $logoPath;
 			$article->zone_id_zone = $donnees['zone_id_zone'];
 			$article->publication_id_publication = $donnees['publication_id_publication'];
 			$article->users_iduser = Auth::User()->iduser;
 			$article->save();
 			return redirect()->back()->with('message','Votre article est crée avec succès !');
+		}
+
+
+
+		public function zone(){
+			$zone = Zone::get();
+			return view('zone',['zone'=> $zone]);
 		}
 }
---
+++ laravel/app/zoneModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class zoneModel extends Model
+{
+    protected $table = 'zone';
+    protected $primaryKey = 'id_zone';
+    public $timestamps = false;
+}
+}
